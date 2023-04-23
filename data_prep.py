94d84d2cc05cb8d75c0e673eddf481526f1af9fa
stu mals <stuartmals23@gmail.com>
2023-04-23 15:33:59 -0400

    updated data_prep with flag for including procedures ICD9 codes

--- data_prep.py
+++ data_prep.py
@@ -1,9 +1,10 @@
 #### Data Prep ####a
+#%%
 import pandas as pd
 import numpy as np
 import os
 import io
-
+#%%
 # admin_file = io.BytesIO(uploaded['ADMISSIONS.csv'])
 # diag_file = io.BytesIO(uploaded['DIAGNOSES_ICD.csv'])
 # procedure_file = io.BytesIO(uploaded['PROCEDURES_ICD.csv'])
@@ -13,88 +14,144 @@ import io
 # datasets_path = '.\\mimic3_demo_data\\'
 
 
-def data_prep(datasets_path, admin_file, diag_file, procedure_file, prescript_file, drug_file):
-
-    df_adm = pd.read_csv(os.getcwd() + datasets_path + admin_file, dtype=str)
-    df_diags = pd.read_csv(os.getcwd() + datasets_path + diag_file, dtype=str)
-    df_procedures = pd.read_csv(os.getcwd() + datasets_path + procedure_file, dtype=str)
-    df_prescripts = pd.read_csv(os.getcwd() + datasets_path + prescript_file, dtype=str)
-    df_drugs = pd.read_csv(os.getcwd() + datasets_path + drug_file, dtype=str)
-
-    # df_adm = pd.read_csv(admin_file, dtype=str)
-    # df_diags = pd.read_csv(diag_file, dtype=str)
-    # df_procedures = pd.read_csv(procedure_file, dtype=str)
-    # df_prescripts = pd.read_csv(prescript_file, dtype=str)
-    # df_drugs = pd.read_csv(drug_file, dtype=str)
-
-    df_adm['admittime'] = pd.to_datetime(df_adm['admittime'])
-    df_adm['dischtime'] = pd.to_datetime(df_adm['dischtime'])
-    df_adm['num_days'] = (df_adm['dischtime'] - df_adm['admittime']).dt.days + 1
-    df_adm = df_adm.rename(columns={'hospital_expire_flag':'death_flag'})
-
-    #df_diags = df_diags.rename(columns={'icd9_code':'icd9_diag'})
-    #df_diags['icd9_parent'] = df_diags['icd9_code'].apply(lambda x: x[:4] if x[0] == 'E' else x[:3])
-    df_diags['icd9_category'] = df_diags['icd9_code'].apply(convert_to_high_level_icd9)
-    df_diags['icd9_diag'] = df_diags[['seq_num', 'icd9_category']].apply(tuple, axis=1)
-    df_procedures = df_procedures.rename(columns={'icd9_code':'icd9_proced'})
-
+def data_prep(datasets_path, admin_file, diag_file, procedure_file=None, 
+              prescript_file=None, drug_file=None):
+    
+    def sort_list(row):
+            row2 = sorted(row, key=lambda x: int(x[0]))
+            return [x[1] for x in row2]
+    
     icd9_diag_categories = list(range(19))
 
-    df_diags2 = df_diags.loc[:,['subject_id', 'hadm_id', 'icd9_diag']].dropna()
-    df_adm2 = df_adm.loc[:,['hadm_id', 'admittime', 'num_days', 'death_flag']]
-    df = df_diags2.merge(df_adm2, how='left', on='hadm_id')
+    if procedure_file and prescript_file and drug_file:
 
-    df_procedures2 = df_procedures.loc[:,['hadm_id', 'icd9_proced']].dropna()
-    df2 = df.merge(df_procedures2, how='left', on='hadm_id')
+        df_adm = pd.read_csv(os.getcwd() + datasets_path + admin_file, dtype=str)
+        df_diags = pd.read_csv(os.getcwd() + datasets_path + diag_file, dtype=str)
+        df_procedures = pd.read_csv(os.getcwd() + datasets_path + procedure_file, dtype=str)
+        df_prescripts = pd.read_csv(os.getcwd() + datasets_path + prescript_file, dtype=str)
+        df_drugs = pd.read_csv(os.getcwd() + datasets_path + drug_file, dtype=str)
 
-    df_prescripts2 = df_prescripts.loc[:,['hadm_id', 'ndc']].dropna()
-    df_prescripts2 = df_prescripts2[df_prescripts2['ndc']!='0']
-    df3 = df2.merge(df_prescripts2, how='left', on='hadm_id')
+        # df_adm = pd.read_csv(admin_file, dtype=str)
+        # df_diags = pd.read_csv(diag_file, dtype=str)
+        # df_procedures = pd.read_csv(procedure_file, dtype=str)
+        # df_prescripts = pd.read_csv(prescript_file, dtype=str)
+        # df_drugs = pd.read_csv(drug_file, dtype=str)
 
-    df_drugs2 = df_drugs.loc[:,['hadm_id', 'drg_code']].dropna()
-    df4 = df3.merge(df_drugs2, how='left', on='hadm_id')
+        df_adm['admittime'] = pd.to_datetime(df_adm['admittime'])
+        df_adm['dischtime'] = pd.to_datetime(df_adm['dischtime'])
+        df_adm['num_days'] = (df_adm['dischtime'] - df_adm['admittime']).dt.days + 1
+        df_adm = df_adm.rename(columns={'hospital_expire_flag':'death_flag'})
 
-    df5 = df4.groupby(['subject_id', 'hadm_id']).agg(set)
+        #df_diags = df_diags.rename(columns={'icd9_code':'icd9_diag'})
+        #df_diags['icd9_parent'] = df_diags['icd9_code'].apply(lambda x: x[:4] if x[0] == 'E' else x[:3])
+        df_diags['icd9_category'] = df_diags['icd9_code'].apply(convert_to_high_level_icd9)
+        df_diags['icd9_diag'] = df_diags[['seq_num', 'icd9_category']].apply(tuple, axis=1)
+        df_procedures = df_procedures.rename(columns={'icd9_code':'icd9_proced'})
 
-    def sort_list(row):
-        row2 = sorted(row, key=lambda x: int(x[0]))
-        return [x[1] for x in row2]
+        df_diags2 = df_diags.loc[:,['subject_id', 'hadm_id', 'icd9_diag']].dropna()
+        df_adm2 = df_adm.loc[:,['hadm_id', 'admittime', 'num_days', 'death_flag']]
+        df = df_diags2.merge(df_adm2, how='left', on='hadm_id')
 
-    df5['icd9_diag'] = df5['icd9_diag'].apply(list)
-    df5['icd9_diag'] = df5['icd9_diag'].apply(sort_list)
-    df5['icd9_diag'] = df5['icd9_diag'].apply(lambda x: list(dict.fromkeys(x)))
-    df5['icd9_proced'] = df5['icd9_proced'].apply(list)
-    df5['ndc'] = df5['ndc'].apply(list)
-    df5['drg_code'] = df5['drg_code'].apply(list)
-    #df5['admittime'] = df5['admittime'].apply(lambda x: list(x)[0].strftime("%Y-%m-%d"))
-    df5['admittime'] = df5['admittime'].apply(lambda x: list(x)[0])
-    df5['num_days'] = df5['num_days'].apply(lambda x: list(x)[0]).astype(str)
+        df_procedures2 = df_procedures.loc[:,['hadm_id', 'icd9_proced']].dropna()
+        df2 = df.merge(df_procedures2, how='left', on='hadm_id')
 
-    df6 = df5.reset_index()
-    df6['death_flag'] = df6['death_flag'].apply(lambda x: list(x)[0])
+        df_prescripts2 = df_prescripts.loc[:,['hadm_id', 'ndc']].dropna()
+        df_prescripts2 = df_prescripts2[df_prescripts2['ndc']!='0']
+        df3 = df2.merge(df_prescripts2, how='left', on='hadm_id')
 
-    data = []
-    patient_ids = df6['subject_id'].unique()
-    for p_id in patient_ids:
-        patient = {}
-        df_p_id = df6[df6['subject_id']==p_id].sort_values('admittime')
-        adm_ids = df_p_id.loc[:,'hadm_id']
-        visits = []
-        for adm_id in adm_ids:
-            df_visit = df6[(df6['subject_id']==p_id) & (df6['hadm_id']==adm_id)]
-            visit = {}
-            visit['admittime'] = df_visit['admittime'].values[0]
-            visit['num_days'] = df_visit['num_days'].values[0]
-            visit['death_flag'] = df_visit['death_flag'].values[0]
-            visit['icd9_diags'] = df_visit['icd9_diag'].values[0]
-            visit['icd9_procedures'] = df_visit['icd9_proced'].values[0]
-            visit['prescriptions'] = df_visit['ndc'].values[0]
-            visit['drugs'] = df_visit['drg_code'].values[0]
-            visits.append(visit)
-        patient[p_id] = visits
-        data.append(patient)
+        df_drugs2 = df_drugs.loc[:,['hadm_id', 'drg_code']].dropna()
+        df4 = df3.merge(df_drugs2, how='left', on='hadm_id')
 
-    return (data, icd9_diag_categories)
+        df5 = df4.groupby(['subject_id', 'hadm_id']).agg(set)
+
+        df5['icd9_diag'] = df5['icd9_diag'].apply(list)
+        df5['icd9_diag'] = df5['icd9_diag'].apply(sort_list)
+        df5['icd9_diag'] = df5['icd9_diag'].apply(lambda x: list(dict.fromkeys(x)))
+        df5['icd9_proced'] = df5['icd9_proced'].apply(list)
+        df5['ndc'] = df5['ndc'].apply(list)
+        df5['drg_code'] = df5['drg_code'].apply(list)
+        #df5['admittime'] = df5['admittime'].apply(lambda x: list(x)[0].strftime("%Y-%m-%d"))
+        df5['admittime'] = df5['admittime'].apply(lambda x: list(x)[0])
+        df5['num_days'] = df5['num_days'].apply(lambda x: list(x)[0]).astype(str)
+
+        df6 = df5.reset_index()
+        df6['death_flag'] = df6['death_flag'].apply(lambda x: list(x)[0])
+
+        data = []
+        patient_ids = df6['subject_id'].unique()
+        for p_id in patient_ids:
+            patient = {}
+            df_p_id = df6[df6['subject_id']==p_id].sort_values('admittime')
+            adm_ids = df_p_id.loc[:,'hadm_id']
+            visits = []
+            for adm_id in adm_ids:
+                df_visit = df6[(df6['subject_id']==p_id) & (df6['hadm_id']==adm_id)]
+                visit = {}
+                visit['admittime'] = df_visit['admittime'].values[0]
+                visit['num_days'] = df_visit['num_days'].values[0]
+                visit['death_flag'] = df_visit['death_flag'].values[0]
+                visit['icd9_diags'] = df_visit['icd9_diag'].values[0]
+                visit['icd9_procedures'] = df_visit['icd9_proced'].values[0]
+                visit['prescriptions'] = df_visit['ndc'].values[0]
+                visit['drugs'] = df_visit['drg_code'].values[0]
+                visits.append(visit)
+            patient[p_id] = visits
+            data.append(patient)
+
+        return (data, icd9_diag_categories)
+    
+    else:
+        df_adm = pd.read_csv(os.getcwd() + datasets_path + admin_file, dtype=str)
+        df_diags = pd.read_csv(os.getcwd() + datasets_path + diag_file, dtype=str)
+        df_diags = df_diags.dropna()
+
+        df_adm.columns = df_adm.columns.str.lower()
+        df_diags.columns = df_diags.columns.str.lower()
+
+        df_adm['admittime'] = pd.to_datetime(df_adm['admittime'])
+        df_adm['dischtime'] = pd.to_datetime(df_adm['dischtime'])
+        df_adm['num_days'] = (df_adm['dischtime'] - df_adm['admittime']).dt.days + 1
+        df_adm = df_adm.rename(columns={'hospital_expire_flag':'death_flag'})
+        df_diags['icd9_code'] = df_diags['icd9_code'].astype(str)
+
+        df_diags['icd9_category'] = df_diags['icd9_code'].apply(convert_to_high_level_icd9)
+        df_diags['icd9_diag'] = df_diags[['seq_num', 'icd9_category']].apply(tuple, axis=1)
+
+        df_diags2 = df_diags.loc[:,['subject_id', 'hadm_id', 'icd9_diag']].dropna()
+        df_adm2 = df_adm.loc[:,['hadm_id', 'admittime', 'num_days', 'death_flag']]
+        df = df_diags2.merge(df_adm2, how='left', on='hadm_id')
+
+        df2 = df.groupby(['subject_id', 'hadm_id']).agg(set)
+
+        df2['icd9_diag'] = df2['icd9_diag'].apply(list)
+        df2['icd9_diag'] = df2['icd9_diag'].apply(sort_list)
+        df2['icd9_diag'] = df2['icd9_diag'].apply(lambda x: list(dict.fromkeys(x)))
+        df2['admittime'] = df2['admittime'].apply(lambda x: list(x)[0])
+        df2['num_days'] = df2['num_days'].apply(lambda x: list(x)[0]).astype(str)
+
+        df3 = df2.reset_index()
+        df3['death_flag'] = df3['death_flag'].apply(lambda x: list(x)[0])
+
+        data = []
+        patient_ids = df3['subject_id'].unique()
+        for p_id in patient_ids:
+            patient = {}
+            df_p_id = df3[df3['subject_id']==p_id].sort_values('admittime')
+            adm_ids = df_p_id.loc[:,'hadm_id']
+            visits = []
+            for adm_id in adm_ids:
+                df_visit = df3[(df3['subject_id']==p_id) & (df3['hadm_id']==adm_id)]
+                visit = {}
+                visit['admittime'] = df_visit['admittime'].values[0]
+                visit['num_days'] = df_visit['num_days'].values[0]
+                visit['death_flag'] = df_visit['death_flag'].values[0]
+                visit['icd9_diags'] = df_visit['icd9_diag'].values[0]
+                visits.append(visit)
+            patient[p_id] = visits
+            data.append(patient)
+
+        return (data, icd9_diag_categories)
+
 
 def get_last_code(data):
     last_codes = []
@@ -105,7 +162,6 @@ def get_last_code(data):
     return last_codes
 
 def dataset_encoding(data, icd9_categories, max_visits):
-    max_visits = 5
     dataset = np.zeros((len(data), len(icd9_categories), max_visits))
     for i, patient in enumerate(data):
         for p_id in patient.keys():
@@ -164,5 +220,8 @@ def convert_to_high_level_icd9(icd9_code):
         return 16
     elif 'E00' <= k <= 'E99':
         return 17
-    elif 'V01' <= k <= 'V90':
+    elif 'V01' <= k <= 'V99':
         return 18
+    
+#%%
+
